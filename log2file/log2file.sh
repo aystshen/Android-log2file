1a14f26a42a0a4ea5119aa7561642246ae8cc4b3
aystshen <ayst_shen@qq.com>
2019-09-05 12:20:39 +0800

    重构，解决最大日志数据限制无效问题

--- log2file/log2file.sh
+++ log2file/log2file.sh
@@ -5,49 +5,53 @@ SDCARD_PATH=/sdcard
 FILE_MAX_SIZE=$((1024*1024*20))
 LOGCAT_PID=-1
 KMSG_PID=-1
-MAX_INDEX=4
+MAX_INDEX=10
 
 ANDROID_LOG_FILE_PATH=${SDCARD_PATH}/log/android
-ANDROID_LOG_FILE=${ANDROID_LOG_FILE_PATH}/android.log
 KERNEL_LOG_FILE_PATH=${SDCARD_PATH}/log/kernel
-KERNEL_LOG_FILE=${KERNEL_LOG_FILE_PATH}/kernel.log
-
-while [ ! -d ${SDCARD_PATH}/Android ] ; do
-	echo "log2file: ${SDCARD_PATH}/Android no exist and wait 3s."
-	sleep 3
-done
-
 ANDROID_INDEX_FILE=${ANDROID_LOG_FILE_PATH}/index.txt
-if [ -f ${ANDROID_INDEX_FILE} ]
-then
-	ANDROID_INDEX=`cat ${ANDROID_INDEX_FILE}`
-	ANDROID_INDEX=`busybox expr ${ANDROID_INDEX} + 1`
-	echo "${ANDROID_INDEX}" > ${ANDROID_INDEX_FILE}
-else
-	ANDROID_INDEX=0
-	echo "0" > ${ANDROID_INDEX_FILE}
-fi
-
 KERNEL_INDEX_FILE=${KERNEL_LOG_FILE_PATH}/index.txt
-if [ -f ${KERNEL_INDEX_FILE} ]
-then
-	KERNEL_INDEX=`cat ${KERNEL_INDEX_FILE}`
-	KERNEL_INDEX=`busybox expr ${KERNEL_INDEX} + 1`
+
+wait_boot_complete() {
+	while [ ! -d ${SDCARD_PATH}/Android ] ; do
+		echo "log2file: ${SDCARD_PATH}/Android no exist and wait 3s."
+		sleep 3
+	done
+
+	if [ ! -d ${ANDROID_LOG_FILE_PATH} ]; then
+		mkdir -p ${ANDROID_LOG_FILE_PATH}
+	fi
+
+	if [ ! -d ${KERNEL_LOG_FILE_PATH} ]; then
+		mkdir -p ${KERNEL_LOG_FILE_PATH}
+	fi
+}
+
+inc_logcat_index() {
+	if [ -f ${ANDROID_INDEX_FILE} ]; then
+		ANDROID_INDEX=`cat ${ANDROID_INDEX_FILE}`
+		ANDROID_INDEX=`busybox expr ${ANDROID_INDEX} + 1`
+		if [ ${ANDROID_INDEX} -gt ${MAX_INDEX} ]; then
+			ANDROID_INDEX=0
+		fi
+	else
+		ANDROID_INDEX=0
+	fi
+	echo "${ANDROID_INDEX}" > ${ANDROID_INDEX_FILE}
+}
+
+inc_kmsg_index() {
+	if [ -f ${KERNEL_INDEX_FILE} ]; then
+		KERNEL_INDEX=`cat ${KERNEL_INDEX_FILE}`
+		KERNEL_INDEX=`busybox expr ${KERNEL_INDEX} + 1`
+		if [ ${KERNEL_INDEX} -gt ${MAX_INDEX} ]; then
+			KERNEL_INDEX=0
+		fi
+	else
+		KERNEL_INDEX=0
+	fi
 	echo "${KERNEL_INDEX}" > ${KERNEL_INDEX_FILE}
-else
-	KERNEL_INDEX=0
-	echo "0" > ${KERNEL_INDEX_FILE}
-fi
-
-if [ ! -d ${ANDROID_LOG_FILE_PATH} ]
-then
-    mkdir -p ${ANDROID_LOG_FILE_PATH}
-fi
-
-if [ ! -d ${KERNEL_LOG_FILE_PATH} ]
-then
-    mkdir -p ${KERNEL_LOG_FILE_PATH}
-fi
+}
 
 logcat2file() {
 	if [ ${LOGCAT_PID} != -1 ]; then
@@ -80,41 +84,38 @@ kmsg2file() {
 	echo "log2file: kmsg2file pid=${KMSG_PID}, file=${KERNEL_LOG_FILE}"
 }
 
-kmsg2file
-logcat2file
-while true ; do
-	afilesize=`ls -l ${ANDROID_LOG_FILE} | busybox awk '{ print $5 }'`
-	kfilesize=`ls -l ${KERNEL_LOG_FILE} | busybox awk '{ print $5 }'`
-	echo "log2file: ${ANDROID_LOG_FILE} size=${afilesize}, FILE_MAX_SIZE=${FILE_MAX_SIZE}"
-	echo "log2file: ${KERNEL_LOG_FILE} size=${kfilesize}, FILE_MAX_SIZE=${FILE_MAX_SIZE}"
+run() {
+	inc_logcat_index
+	inc_kmsg_index
 
-	if [ ${afilesize} -gt ${FILE_MAX_SIZE} ]
-	then
-		if [ ${ANDROID_INDEX} -gt ${MAX_INDEX} ]
+	ANDROID_LOG_FILE=${ANDROID_LOG_FILE_PATH}/android_${ANDROID_INDEX}.log
+	KERNEL_LOG_FILE=${KERNEL_LOG_FILE_PATH}/kernel_${KERNEL_INDEX}.log
+
+	kmsg2file
+	logcat2file
+	while true ; do
+		afilesize=`ls -l ${ANDROID_LOG_FILE} | busybox awk '{ print $5 }'`
+		kfilesize=`ls -l ${KERNEL_LOG_FILE} | busybox awk '{ print $5 }'`
+		#echo "log2file: ${ANDROID_LOG_FILE} size=${afilesize}, FILE_MAX_SIZE=${FILE_MAX_SIZE}"
+		#echo "log2file: ${KERNEL_LOG_FILE} size=${kfilesize}, FILE_MAX_SIZE=${FILE_MAX_SIZE}"
+
+		if [ ${afilesize} -gt ${FILE_MAX_SIZE} ]
 		then
-			ANDROID_INDEX=0
-		else
-			ANDROID_INDEX=`busybox expr ${ANDROID_INDEX} + 1`
+			inc_logcat_index
+			logcat2file
 		fi
-		echo "${ANDROID_INDEX}" > ${ANDROID_INDEX_FILE}
 		
-		logcat2file
-	fi
-	
-	if [ ${kfilesize} -gt ${FILE_MAX_SIZE} ]
-	then
-		if [ ${KERNEL_INDEX} -gt ${MAX_INDEX} ]
+		if [ ${kfilesize} -gt ${FILE_MAX_SIZE} ]
 		then
-			KERNEL_INDEX=0
-		else
-			KERNEL_INDEX=`busybox expr ${KERNEL_INDEX} + 1`
+			inc_kmsg_index
+			kmsg2file
 		fi
-		echo "${KERNEL_INDEX}" > ${KERNEL_INDEX_FILE}
 		
-		kmsg2file
-	fi
-	
-	sleep 3
-done
+		sleep 10
+	done
+}
+
+wait_boot_complete
+run
 
 exit 0
